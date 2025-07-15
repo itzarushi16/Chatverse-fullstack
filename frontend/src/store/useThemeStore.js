995248bcff7a71ca74446365ee1c6f5b0d0307b1
ARUSHI ALONE <137710409+itzarushi16@users.noreply.github.com>
2025-07-16 02:14:24 +0530

    Update useThemeStore.js

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-
+//use theme store
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("chat-theme") || "coffee",
   setTheme: (theme) => {
