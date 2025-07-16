4bbbdb73a773fb0372386b799088718bfb992aa4
ARUSHI ALONE <137710409+itzarushi16@users.noreply.github.com>
2025-07-17 00:59:10 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//axios page
 export const axiosInstance=axios.create({
   baseURL: import.meta.env.MODE==="development"? "http://localhost:5001/api":"/api",
   withCredentials:true,
