4515b6e6d6451104319adf3298b5e5e0d84912c5
ARUSHI ALONE <137710409+itzarushi16@users.noreply.github.com>
2025-07-19 19:50:15 +0530

    Update NoChatSelected.jsx

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -16,6 +16,8 @@ const NoChatSelected = () => {
           </div>
         </div>
 
+        
+
         {/* Welcome Text */}
         <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
         <p className="text-base-content/60">
