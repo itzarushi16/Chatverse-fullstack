9316927c37a98c6eb2fbdc937b19635c747ad601
Arushi Aloné <137710409+itzarushi16@users.noreply.github.com>
2025-07-28 19:42:01 +0530

    Update AuthImagePattern.jsx

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -4,6 +4,9 @@ const AuthImagePattern = ({ title, subtitle }) => {
         <div className="max-w-md text-center">
           <div className="grid grid-cols-3 gap-3 mb-8">
             {[...Array(9)].map((_, i) => (
+
+
+
           
               <div
                 key={i}
